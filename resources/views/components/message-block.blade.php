ac12126b56773fe7d6d006d732db1142a0198da9
aac2324 <161326407+aac2324@users.noreply.github.com>
2025-10-06 21:11:56 +0200

    changing Review submitted message text to a darker color for visibility

--- resources/views/components/message-block.blade.php
+++ resources/views/components/message-block.blade.php
@@ -1,5 +1,5 @@
 @if(session()->has('specialMessage'))
-    <div class="bg-green-50 p-2 border border-green-500 text-500">
+    <div class="bg-green-50 p-2 border border-green-500 text-black rounded mb-4">
         {{ session()->get('specialMessage') }}
     </div>
 @endif
