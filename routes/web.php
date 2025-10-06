b6f9d90b7c5cc696544f5afb7a395c2d9449e0ff
aac2324 <161326407+aac2324@users.noreply.github.com>
2025-10-06 14:09:30 +0200

    name changes in routes

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,10 @@ Route::get('/', [\App\Http\Controllers\WelcomeController::class, 'index'])->name
 Route::get('authors', [AuthorController::class, 'index']);
 Route::get('authors/{id}', [AuthorController::class, 'show']);
 
-Route::get('articles', [\App\Http\Controllers\ArticleController::class, 'index'] )->name('articles.index');
-Route::get('articles/{id}', [\App\Http\Controllers\ArticleController::class, 'show'])->name('articles.show');
+Route::get('articles', [\App\Http\Controllers\ArticleController::class, 'index'] )->name('events.index');
+Route::get('articles/{id}', [\App\Http\Controllers\ArticleController::class, 'show'])->name('events.show');
 
-Route::post('comments', [\App\Http\Controllers\CommentController::class, 'store'])->name('comments.store');
+Route::post('comments', [\App\Http\Controllers\CommentController::class, 'store'])->name('reviews.store');
 
 
 require __DIR__.'/auth.php';
@@ -23,10 +23,10 @@ require __DIR__.'/auth.php';
  * Routes that require authentication
  */
 Route::middleware('auth')->group(function () {
-    Route::get('management/articles', [\App\Http\Controllers\ArticleManagementController::class, 'index'])->name('articles.index');
-    Route::get('management/articles/create', [\App\Http\Controllers\ArticleManagementController::class, 'create'])->name('articles.create');
-    Route::post('management/articles', [\App\Http\Controllers\ArticleManagementController::class, 'store'])->name('articles.store');
-    Route::get('management/articles/{id}/edit', [\App\Http\Controllers\ArticleManagementController::class, 'edit'])->name('articles.edit');
-    Route::put('management/articles/{id}', [\App\Http\Controllers\ArticleManagementController::class, 'update'])->name('articles.update');
-    Route::delete('management/articles/{id}', [\App\Http\Controllers\ArticleManagementController::class, 'destroy'])->name('articles.destroy');
+    Route::get('management/articles', [\App\Http\Controllers\ArticleManagementController::class, 'index'])->name('events.index');
+    Route::get('management/articles/create', [\App\Http\Controllers\ArticleManagementController::class, 'create'])->name('events.create');
+    Route::post('management/articles', [\App\Http\Controllers\ArticleManagementController::class, 'store'])->name('events.store');
+    Route::get('management/articles/{id}/edit', [\App\Http\Controllers\ArticleManagementController::class, 'edit'])->name('events.edit');
+    Route::put('management/articles/{id}', [\App\Http\Controllers\ArticleManagementController::class, 'update'])->name('events.update');
+    Route::delete('management/articles/{id}', [\App\Http\Controllers\ArticleManagementController::class, 'destroy'])->name('events.destroy');
 });
